bb0225cac573d9f4d0bdae481a700f9fbfef47bd
Inji Zakariyayeva <incizekeriyeyeva@gmail.com>
2023-01-07 10:04:49 +0400

    initial commit

---
+++ BoolkyBookApp.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BoolkyBookApp.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
