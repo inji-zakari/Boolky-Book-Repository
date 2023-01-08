56250a4ef49ac7ee8195a30e529f9e6f4b67a34f
Inji Zakariyayeva <incizekeriyeyeva@gmail.com>
2023-01-08 20:57:01 +0400

    completed CRUD Operations

--- BoolkyBookApp.WebUI/Controllers/CategoryController.cs
+++ BoolkyBookApp.WebUI/Controllers/CategoryController.cs
@@ -25,13 +25,74 @@ namespace BoolkyBook.WebUI.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category category)
         {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "Display order can not match Name");
+            }
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(category);
                 _db.SaveChanges();
+                TempData["success"] = "Category added successfully!";
                 return RedirectToAction("Index");
             }
-            return View();
+            return View(category);
+        }
+        [HttpGet]
+        public IActionResult Update(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Update(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The Name and Display Order should not match each other!");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                TempData["success"] = "category updated successfully!";
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _db.Categories.FirstOrDefault(x => x.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Delete(Category category)
+        {
+            if (category != null)
+            {
+                _db.Categories.Remove(category);
+                _db.SaveChanges();
+                TempData["success"] = "Category deleted successfully!";
+                return RedirectToAction("Index");
+            }
+            return View(category);
         }
     }
 }
